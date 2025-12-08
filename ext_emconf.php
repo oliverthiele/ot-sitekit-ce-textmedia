3f8f05d36ba78752517cf2d26cc62ef6997fb187
Oliver Thiele <mail@oliver-thiele.de>
2025-12-08 16:45:54 +0100

    [FEATURE] Adds configuration for text media component
    Introduces a new configuration for the text media component, including its dependency on a base kit.
    
    Updates the main configuration file to reflect the component's author and company details.
    
    Enhances the overall setup by ensuring proper dependency management.

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,11 +1,12 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = [
+$EM_CONF['ot_sitekitcetextmedia'] = [
     'title' => 'Text & Medien',
     'description' => 'CE Textmedia',
     'category' => 'frontend',
     'author' => 'Oliver Thiele',
     'author_email' => 'mail@oliver-thiele.de',
+    'author_company' => 'Web Development Oliver Thiele',
     'state' => 'stable',
     'version' => '1.0.0',
     'constraints' => [
